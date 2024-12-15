8b7b8016217f79316d79ff68fafc176e233e2e83
Ryan <ryanbprog@gmail.com>
2024-12-15 22:02:17 +0000

    Routes for editing users shortcuts completed

--- backend/controllers/users/usersControllers.ts
+++ backend/controllers/users/usersControllers.ts
@@ -3,7 +3,7 @@ import UserModel from "../../db/models/UserModel";
 import catchErrorMessage from "../../utils/catchErrorMessage";
 import { TUserTokenRequest } from "../../types/requestBodyControllersTypes";
 
-async function getUserData(req: TUserTokenRequest, res: Response) {
+export const getUserData = async (req: TUserTokenRequest, res: Response) => {
   try {
     const userId = req.user?.userId;
 
@@ -20,6 +20,74 @@ async function getUserData(req: TUserTokenRequest, res: Response) {
     catchErrorMessage("Error in getUserData", error);
     res.status(500).json({ error: "Internal server error" });
   }
-}
+};
 
-export default getUserData;
+export const addUserShortcut = async (
+  req: TUserTokenRequest,
+  res: Response
+) => {
+  try {
+    const shortcutId = req.body.shortcutId;
+    if (!shortcutId) {
+      res.status(400).json({ error: "Shortcut ID is required" });
+      return;
+    }
+
+    const userId = req.user?.userId;
+    if (!userId) {
+      res.status(401).json({ error: "User not authenticated" });
+      return;
+    }
+
+    const updatedUser = await UserModel.findOneAndUpdate(
+      { _id: userId },
+      { $push: { "custom.shortcuts": shortcutId } },
+      { new: true, runValidators: true }
+    ).select("-password");
+
+    if (!updatedUser) {
+      res.status(400).json({ error: "User not found" });
+      return;
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    catchErrorMessage("Error in addUserShortcut", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const deleteUserShortcut = async (
+  req: TUserTokenRequest,
+  res: Response
+) => {
+  try {
+    const shortcutId = req.params.shortcutId;
+    if (!shortcutId) {
+      res.status(400).json({ error: "Shortcut ID is required" });
+      return;
+    }
+
+    const userId = req.user?.userId;
+    if (!userId) {
+      res.status(401).json({ error: "User not authenticated" });
+      return;
+    }
+
+    const updatedUser = await UserModel.findOneAndUpdate(
+      { _id: userId },
+      { $pull: { "custom.shortcuts": shortcutId } },
+      { new: true, runValidators: true }
+    ).select("-password");
+
+    if (!updatedUser) {
+      res.status(400).json({ error: "User not found" });
+      return;
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    catchErrorMessage("Error in deleteUserShortcut", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/routes/usersRoutes.ts
+++ backend/routes/usersRoutes.ts
@@ -1,9 +1,19 @@
 import express from "express";
-import getUserData from "../controllers/users/usersControllers";
+import {
+  getUserData,
+  addUserShortcut,
+  deleteUserShortcut,
+} from "../controllers/users/usersControllers";
 import checkUserToken from "../middleware/checkUserToken";
 
 const router = express.Router();
 
 router.get("/user", checkUserToken, getUserData);
+router.post("/user/shortcuts", checkUserToken, addUserShortcut);
+router.delete(
+  "/user/shortcuts/:shortcutId",
+  checkUserToken,
+  deleteUserShortcut
+);
 
 export default router;
