df6b4f2e060dde8c40894e243e3bed2babbb5188
Ryan <ryanbprog@gmail.com>
2024-12-15 22:37:35 +0000

    login and sign up pages complete

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,7 @@
+import Login from "./login/page";
+import Signup from "./signup/page";
+
 export default function Home() {
-  return <main>dashboard</main>;
+  return <Login />;
+  // return <Signup />;
 }
---
+++ frontend/app/signup/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+import Link from "next/link";
+
+const userDataTemplate = {
+  fullName: "",
+  email: "",
+  confirmPassword: "",
+  password: "",
+};
+
+export default function Signup() {
+  const [userData, setUserData] = useState(userDataTemplate);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    // send request to server
+  }
+  return (
+    <div className="pt-20">
+      <h1 className="text-center font-bold text-3xl pb-8">User SignUp</h1>
+      <form onSubmit={handleSubmit} className="mx-auto w-96 grid gap-4">
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
+          <input
+            type="text"
+            className="grow"
+            placeholder="Full Name"
+            value={userData.fullName}
+            onChange={(e) =>
+              setUserData({ ...userData, fullName: e.target.value })
+            }
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
+          <input
+            type="text"
+            className="grow"
+            placeholder="Email"
+            value={userData.email}
+            onChange={(e) =>
+              setUserData({ ...userData, email: e.target.value })
+            }
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input
+            type="password"
+            className="grow"
+            placeholder="Password"
+            value={userData.password}
+            onChange={(e) =>
+              setUserData({ ...userData, password: e.target.value })
+            }
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input
+            type="password"
+            className="grow"
+            placeholder="Confirm Password"
+            value={userData.confirmPassword}
+            onChange={(e) =>
+              setUserData({ ...userData, confirmPassword: e.target.value })
+            }
+          />
+        </label>
+      </form>
+      <span className="block text-center pt-8">
+        Already have an account?{" "}
+        <Link href="/login" className="underline hover:text-gray-300">
+          Login
+        </Link>
+      </span>
+    </div>
+  );
+}
