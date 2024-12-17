8697ed68c4ec807b07c5ff6279be50293f8dea05
Ryan <ryanbprog@gmail.com>
2024-12-17 16:41:00 +0000

    hook created for fetching logged in users data

---
+++ frontend/app/hooks/fetchUserData.ts
@@ -0,0 +1,15 @@
+export async function fetchUserData() {
+  try {
+    const res = await fetch("http://localhost:4040/api/users/user", {
+      credentials: "include",
+    });
+    const userData = await res.json();
+    if (userData.error) {
+      throw new Error(userData.error);
+    }
+    return userData;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
