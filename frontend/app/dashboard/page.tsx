ce60c42cdee0352b719611674772d4b137c5104d
Ryan <ryanbprog@gmail.com>
2024-12-19 08:59:51 +0000

    moved fetchUserData, dashboard page now a server component

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,44 +1,16 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { fetchUserData } from "../hooks/fetchUserData";
+import { fetchUserData } from "./utils/fetchUserData";
 import { demoData } from "./demoData";
 import { createShortcutTable } from "./utils/createShortcutTable";
 import TableRow from "./components/TableRow";
 
-export default function Dashboard() {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-  const [fullName, setFullName] = useState("");
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const userData = await fetchUserData();
-      if (!userData) {
-        router.push(`/login`);
-        return;
-      }
-      setFullName(userData.fullName);
-      setLoading(false);
-    };
-    fetchUser();
-  }, []);
-
-  if (loading) {
-    return (
-      <span className="font-bold text-2xl block text-center mx-auto mt-20">
-        <span className="loading loading-spinner mr-3"></span>loading...
-      </span>
-    );
-  }
-
+export default async function Dashboard() {
+  const userData = await fetchUserData();
   const userShortcuts = createShortcutTable(demoData);
 
   return (
     <div>
       <h1 className="font-bold text-2xl text-center pt-10 capitalize">
-        Welcome {fullName}
+        Welcome {userData.fullName}
       </h1>
       <div>
         {userShortcuts.map((category) => {
--- frontend/app/dashboard/utils/fetchUserData.ts
+++ frontend/app/dashboard/utils/fetchUserData.ts
@@ -1,7 +1,12 @@
+import { cookies } from "next/headers";
+
 export async function fetchUserData() {
   try {
+    const cookieStore = await cookies();
+
     const res = await fetch("http://localhost:4040/api/user", {
-      credentials: "include",
+      method: "GET",
+      headers: { Cookie: cookieStore.toString() },
     });
     const userData = await res.json();
     if (userData.error) {
