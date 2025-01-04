21b4b9a968690736fb31e0ad4344ed50f4ece72b
Ryan <ryanbprog@gmail.com>
2025-01-04 13:54:27 +0000

    added links to footer nav tags

--- frontend/app/components/root-footer/Footer.tsx
+++ frontend/app/components/root-footer/Footer.tsx
@@ -19,24 +19,28 @@ export default function Footer() {
         </p>
       </aside>
       <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
         <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+        <a href="/about" className="link link-hover">
+          About us
+        </a>
+        <a href="/contact" className="link link-hover">
+          Contact
+        </a>
+        <a href="/pricing" className="link link-hover">
+          Pricing
+        </a>
       </nav>
       <nav>
         <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        <a href="/terms" className="link link-hover">
+          Terms of use
+        </a>
+        <a href="/privacy-policy" className="link link-hover">
+          Privacy policy
+        </a>
+        <a href="/cookie-policy" className="link link-hover">
+          Cookie policy
+        </a>
       </nav>
     </footer>
   );
