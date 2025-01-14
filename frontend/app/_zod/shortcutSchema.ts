ce7f76a0779a245895dc656f4520fe36d7ff39fc
Ryan <ryanbprog@gmail.com>
2025-01-14 16:01:54 +0800

    created a schema for creating a shortcut

---
+++ frontend/app/_zod/shortcutSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const shortcutSchema = z.object({
+  shortDescription: z
+    .string()
+    .max(40, "shortDescription is longer than 40 characters"),
+  description: z.string(),
+  keys: z
+    .array(z.string().min(1, "String must not be empty"))
+    .nonempty("At least one key is required"),
+  type: z.string(),
+});
