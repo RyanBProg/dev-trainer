c85ce282674222060625eebdf9ca8972797240a3
Ryan <ryanbprog@gmail.com>
2025-01-17 15:43:56 +0800

    added an admin check layer to allow for a suspense state ui

--- frontend/app/dashboard/admin/page.tsx
+++ frontend/app/dashboard/admin/page.tsx
@@ -1,31 +1,20 @@
-import EditShortcut from "./_components/EditShortcut";
-import CreateShortcutForm from "./_components/CreateShortcutForm";
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
+import AdminCheckLayer from "./_components/AdminCheckLayer";
+import { Suspense } from "react";
 
-export default async function Admin() {
-  const cookieStore = await cookies();
-
-  const res = await fetch("http://localhost:4040/api/user", {
-    method: "GET",
-    headers: { Cookie: cookieStore.toString() },
-  });
-
-  const resData = await res.json();
-  if (resData.error) redirect("/dashboard");
-  if (!resData.isAdmin) redirect("/dashboard");
-
+export default function Admin() {
   return (
     <div className="px-8 container mx-auto">
       <h1 className="font-bold text-2xl text-center mt-10 mb-5 capitalize">
         Admin Panel
       </h1>
-      <h2 className="mb-4 font-semibold text-lg">Create a Shortcut</h2>
-      <CreateShortcutForm />
-
-      <hr className="mt-10 border-neutral-600" />
-
-      <EditShortcut />
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center mt-28">
+            <span className="loading loading-spinner loading-sm"></span>
+          </div>
+        }>
+        <AdminCheckLayer />
+      </Suspense>
     </div>
   );
 }
