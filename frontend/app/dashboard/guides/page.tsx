d6b55e0ec52abe5c3285e10542f362fe5552a07f
Ryan <ryanbprog@gmail.com>
2025-01-18 12:43:17 +0800

    added coming soon template page to routes

--- frontend/app/dashboard/guides/page.tsx
+++ frontend/app/dashboard/guides/page.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import roadworkImage from "@/app/_assets/images/roadwork-sign.png";
+import Image from "next/image";
 
 export default function page() {
   return (
-    <div>
-      <h1>Our Guides</h1>
+    <div className="px-8 mt-32">
+      <div className="container mx-auto flex flex-col gap-20 items-center">
+        <Image src={roadworkImage} alt="roadworks icon" />
+        <div className="flex flex-col gap-4 items-center">
+          <span className="badge badge-outline badge-lg">Coming Soon</span>
+          <h1 className="font-bold text-4xl">Guides</h1>
+        </div>
+      </div>
     </div>
   );
 }
