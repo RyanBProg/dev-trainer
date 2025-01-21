c9c2fdeb9472286217d174bcc81a284ade439df8
Ryan <ryanbprog@gmail.com>
2025-01-21 08:20:33 +0800

    added the business theme

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,5 +7,8 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  daisyui: {
+    themes: ["business"],
+  },
   plugins: [daisyui],
 } satisfies Config;
