7bfb7ba086f06f718f78d2328cdf1eb7c6327bfc
Ryan <ryanbprog@gmail.com>
2025-01-23 21:29:47 +0800

    cors

--- backend/server.ts
+++ backend/server.ts
@@ -12,14 +12,13 @@ dotenv.config();
 const PORT = process.env.PORT || 4040;
 
 // CORS setup
-// const corsOptions = {
-//   origin: "https://dev-trainer-frontend-ryans-projects-197c1757.vercel.app",
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-// };
-// app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions)); // Handle preflight requests
-app.use(cors());
+const corsOptions = {
+  origin: "https://dev-trainer-frontend-ryans-projects-197c1757.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+};
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
 app.use(cookieParser());
