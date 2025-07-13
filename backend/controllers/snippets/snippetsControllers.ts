a8b7baadbf27e51c7062e771b3b1483a0d8def6b
Ryan <ryanbprog@gmail.com>
2025-07-13 19:07:38 +0100

    fixed model version typo

--- backend/controllers/snippets/snippetsControllers.ts
+++ backend/controllers/snippets/snippetsControllers.ts
@@ -7,7 +7,7 @@ import { generateSnippetPrompt } from "./utils";
 const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash",
+  model: "gemini-2.0-flash-lite",
   generationConfig: {
     temperature: 0.1,
     topK: 40,
